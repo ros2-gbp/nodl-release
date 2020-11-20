6962afb6e105e20a854ad84051f491956408be86
Ted Kern <ted.kern@canonical.com>
2020-11-20 19:15:57 +0000

    Rebase from 'debian/rolling/nodl_python'

--- test/nodl/test_types.py
+++ test/nodl/test_types.py
@@ -1,32 +1,34 @@
-# Copyright 2020 Canonical Ltd.
-#
-# This program is free software: you can redistribute it and/or modify it under the terms of the
-# GNU Limited General Public License version 3, as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Limited General Public License for more details.
-#
-# You should have received a copy of the GNU Limited General Public License along with
-# this program. If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2020 Canonical, Ltd.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 import nodl.types
 import pytest
-import rclpy.qos
 
 
 @pytest.fixture
 def topic_publisher():
-    return nodl.types.Topic(name='foo', message_type='bar', publisher=True)
+    return nodl.types.Topic(name='foo', message_type='bar', role=nodl.types.PubSubRole.PUBLISHER)
 
 
 def test_action():
-    action_server = nodl.types.Action(name='foo', action_type='bar', server=True)
+    action_server = nodl.types.Action(
+        name='foo', action_type='bar', role=nodl.types.ServerClientRole.SERVER
+    )
     assert action_server.name == 'foo'
     assert action_server.type == 'bar'
-    assert action_server.server
-    assert action_server.qos is not None
+    assert action_server.role == nodl.types.ServerClientRole.SERVER
 
 
 def test_parameter():
@@ -36,17 +38,29 @@ def test_parameter():
 
 
 def test_service():
-    service_client = nodl.types.Service(name='foo', service_type='bar', client=True)
-    assert service_client.name == 'foo'
-    assert service_client.type == 'bar'
-    assert service_client.client
-    assert service_client.qos is not None
+    service = nodl.types.Service(
+        name='foo', service_type='bar', role=nodl.types.ServerClientRole.CLIENT
+    )
+    assert service.name == 'foo'
+    assert service.type == 'bar'
+    assert service.role == nodl.types.ServerClientRole.CLIENT
+
+    service = nodl.types.Service(
+        name='foo', service_type='bar', role=nodl.types.ServerClientRole.SERVER,
+    )
+    assert service.role == nodl.types.ServerClientRole.SERVER
 
 
-def test_topic(topic_publisher):
-    assert topic_publisher.name == 'foo'
-    assert topic_publisher.type == 'bar'
-    assert topic_publisher.publisher
+def test_topic():
+    topic = nodl.types.Topic(name='foo', message_type='bar', role=nodl.types.PubSubRole.PUBLISHER,)
+    assert topic.name == 'foo'
+    assert topic.type == 'bar'
+    assert topic.role == nodl.types.PubSubRole.PUBLISHER
+
+    topic = nodl.types.Topic(
+        name='foo', message_type='bar', role=nodl.types.PubSubRole.SUBSCRIPTION,
+    )
+    assert topic.role == nodl.types.PubSubRole.SUBSCRIPTION
 
 
 def test_representations(topic_publisher):
@@ -54,47 +68,40 @@ def test_representations(topic_publisher):
     assert 'foo' in str(topic_publisher) and 'bar' in str(topic_publisher)
 
 
-def test__as_dict(topic_publisher):
-    assert topic_publisher._as_dict['name'] == 'foo'
-    assert topic_publisher._as_dict['type'] == 'bar'
-    assert isinstance(topic_publisher._as_dict['qos'], dict)
-
-    parameter = nodl.types.Parameter(name='foo', parameter_type='bar')
-    assert 'qos' not in parameter._as_dict
-
-
 def test_equality(topic_publisher):
-    also_topic_publisher = nodl.types.Topic(name='foo', message_type='bar', publisher=True)
+    also_topic_publisher = nodl.types.Topic(
+        name='foo', message_type='bar', role=nodl.types.PubSubRole.PUBLISHER,
+    )
     assert also_topic_publisher == topic_publisher
 
     not_same_topic_publisher = nodl.types.Topic(
-        name='foo', message_type='bar', publisher=True, qos=rclpy.qos.QoSProfile(depth=1)
+        name='bar', message_type='bar', role=nodl.types.PubSubRole.PUBLISHER,
     )
     assert not_same_topic_publisher != topic_publisher
 
+    # Test different roles cause inequality
+    assert nodl.types.Action(
+        name='foo', action_type='bar', role=nodl.types.ServerClientRole.CLIENT
+    ) != nodl.types.Action(name='foo', action_type='bar', role=nodl.types.ServerClientRole.BOTH)
+
 
 def test_same_name_different_interface_type():
-    topic = nodl.types.Topic(name='foo', message_type='bar')
-    service = nodl.types.Service(name='foo', service_type='bar')
+    topic = nodl.types.Topic(name='foo', message_type='bar', role=nodl.types.PubSubRole.PUBLISHER,)
+    service = nodl.types.Service(
+        name='foo', service_type='bar', role=nodl.types.ServerClientRole.SERVER
+    )
     assert topic != service
 
 
-def test_node():
-    topic = nodl.types.Topic(name='foo', message_type='bar')
-    service = nodl.types.Service(name='baz', service_type='woo')
+def test_node(topic_publisher):
+    service = nodl.types.Service(
+        name='baz', service_type='woo', role=nodl.types.ServerClientRole.SERVER
+    )
 
-    node = nodl.types.Node(name='test', executable='toast', topics=[topic], services=[service])
+    node = nodl.types.Node(
+        name='test', executable='toast', topics=[topic_publisher], services=[service]
+    )
     assert node.name == 'test'
     assert node.executable == 'toast'
-    assert node.topics[topic.name] == topic
+    assert node.topics[topic_publisher.name] == topic_publisher
     assert node.services[service.name] == service
-
-
-def test_node__as_dict():
-    topic = nodl.types.Topic(name='foo', message_type='bar')
-    service = nodl.types.Service(name='baz', service_type='woo')
-
-    node = nodl.types.Node(name='test', executable='toast', topics=[topic], services=[service])
-    assert node._as_dict['name'] == node.name
-    assert node._as_dict['executable'] == node.executable
-    assert topic._as_dict in node._as_dict['topics']
