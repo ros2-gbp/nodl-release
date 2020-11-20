d5c585f8025a512dc4f8a314d02ba17840721823
Ted Kern <ted.kern@canonical.com>
2020-11-20 19:14:56 +0000

    Rebase from 'upstream'

--- nodl_python/nodl/_util.py
+++ nodl_python/nodl/_util.py
@@ -1,37 +1,23 @@
-# Copyright 2020 Canonical Ltd.
-#
-# This program is free software: you can redistribute it and/or modify it under the terms of the
-# GNU Limited General Public License version 3, as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-# without even the implied warranties of MERCHANTABILITY, SATISFACTORY QUALITY, or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Limited General Public License for more details.
-#
-# You should have received a copy of the GNU Limited General Public License along with
-# this program. If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2020 Canonical, Ltd.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import distutils.util
-from typing import Dict
 
 from lxml import etree
-from rclpy.qos import QoSProfile
 
 
 def get_bool_attribute(element: etree._Element, attribute: str) -> bool:
     """Access attribute and bool conversion."""
     boolean_string = element.get(attribute, 'False')
     return bool(distutils.util.strtobool(boolean_string))
-
-
-def qos_to_dict(qos: QoSProfile) -> Dict[str, str]:
-    return {
-            'history': qos.history.short_key,
-            'depth': qos.depth,
-            'reliability': qos.reliability.short_key,
-            'durability': qos.durability.short_key,
-            'lifespan': qos.lifespan.nanoseconds,
-            'deadline': qos.deadline.nanoseconds,
-            'liveliness': qos.liveliness.short_key,
-            'liveliness_lease_duration': qos.liveliness_lease_duration.nanoseconds,
-            'avoid_ros_namespace_conventions': qos.avoid_ros_namespace_conventions,
-    }
