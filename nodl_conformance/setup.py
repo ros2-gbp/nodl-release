99fe1e57daf43c0d22d202e0c32726310234bc5a
Emerson Knapp <emerson.b.knapp@gmail.com>
2026-09-04 09:38:14 -0700

    Rebase from 'upstream'

--- nodl_conformance/setup.py
+++ nodl_conformance/setup.py
@@ -6,7 +6,7 @@ package_name = 'nodl_conformance'
 
 setup(
     name=package_name,
-    version='2.0.1',
+    version='2.0.2',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
